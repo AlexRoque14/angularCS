bff407afeee881730de17389d431503e98e9485d
AlexRoque14 <171048@ids.upchiapas.edu.mx>
2020-03-22 12:40:56 -0600

    Para el server

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { WebSocketService } from './services/web-socket.service';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,11 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Cliente';
+
+
+  constructor(public wsService: WebSocketService){
+
+  }
+
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,24 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import {SocketIoConfig, SocketIoModule} from 'ngx-socket-io';
+import { environment } from 'src/environments/environment';
+
+const config: SocketIoConfig = {
+  
+  url : environment.wsUlr, options: {}
+
+};
+
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    SocketIoModule.forRoot(config)
   ],
   providers: [],
   bootstrap: [AppComponent]
